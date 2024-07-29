689e8afbbe95969593f14b8240fd9fd1f22e1a00
ZahraJou <zahrajoulaei@gmail.com>
2024-07-28 23:30:01 -0400

    change the images path

--- vite-react-project/src/components/article/article.jsx
+++ vite-react-project/src/components/article/article.jsx
@@ -6,7 +6,7 @@ export default function Article() {
       <article>
         <time dateTime="2020-12-11">11/12/20</time>
         <h2>On the Street in Brooklyn</h2>
-        <img src="/src/assets/images/blog-image-1.jpg" alt="image-blog" />
+        <img src="/images/blog-image-1.jpg" alt="image-blog" />
 
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet nostrum
@@ -30,7 +30,7 @@ export default function Article() {
       <article>
         <time dateTime="2020-11-11">11/11/20</time>
         <h2>Vintage in Vogue</h2>
-        <img src="/src/assets/images/blog-image-2.jpg" alt="image-blog" />
+        <img src="/images/blog-image-2.jpg" alt="image-blog" />
         <p>
           The city's favorite bear yet again proves his mettle by rescuing a
           young cat from a tree. Witnesses say that Superbear's efforts were not
